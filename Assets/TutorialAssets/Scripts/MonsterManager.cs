523f1e5da0165857e2304783a7b8a3d205992975
Chase <robertchasesommer@gmail.com>
2023-02-06 06:58:03 -0500

    chapter 2 lesson 3

--- Assets/TutorialAssets/Scripts/MonsterManager.cs
+++ Assets/TutorialAssets/Scripts/MonsterManager.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class MonsterManager : MonoBehaviour
 {
 
+    [SerializeField] private int numberOfMonsters;
     [SerializeField] private Transform monsterSpawnPoint;
     [SerializeField] private GameObject[] monsterPrefabs;
+
+    public List<GameObject> monsters;
     
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        
+        for (int i = 0; i < numberOfMonsters; i++)
         {
             int monsterIndex = Random.Range(0, monsterPrefabs.Length);
-            Instantiate(monsterPrefabs[monsterIndex], monsterSpawnPoint.position, monsterSpawnPoint.rotation);
+            GameObject monster = Instantiate(monsterPrefabs[monsterIndex], monsterSpawnPoint.position, monsterSpawnPoint.rotation);
+            monsters.Add(monster);
         }
+        
     }
 
     // Update is called once per frame
