2f931c8fe08d1c4696d3b189f57e06c34e67f3c1
Chase <robertchasesommer@gmail.com>
2023-02-06 08:09:13 -0500

    Missing things. Check answers.

--- Assets/TutorialAssets/Scripts/MonsterManager.cs
+++ Assets/TutorialAssets/Scripts/MonsterManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TutorialAssets.Scripts;
 using UnityEngine;
 
 public class MonsterManager : MonoBehaviour
@@ -7,7 +8,10 @@ public class MonsterManager : MonoBehaviour
 
     [SerializeField] private int numberOfMonsters;
     [SerializeField] private Transform monsterSpawnPoint;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private Transform queuePoint;
     [SerializeField] private GameObject[] monsterPrefabs;
+    [SerializeField] private float waveDifficulty;
 
     public List<GameObject> monsters;
     
@@ -17,11 +21,45 @@ public class MonsterManager : MonoBehaviour
         
         for (int i = 0; i < numberOfMonsters; i++)
         {
-            int monsterIndex = Random.Range(0, monsterPrefabs.Length);
-            GameObject monster = Instantiate(monsterPrefabs[monsterIndex], monsterSpawnPoint.position, monsterSpawnPoint.rotation);
-            monsters.Add(monster);
+            InstantiateMonster();
         }
         
+        MonsterAttacks(0);
+
+        CalculateWaveDifficulty(ref waveDifficulty);
+
+    }
+
+    private void InstantiateMonster()
+    {
+        int monsterIndex = Random.Range(0, monsterPrefabs.Length);
+        GameObject monster =
+            Instantiate(monsterPrefabs[monsterIndex], monsterSpawnPoint.position, monsterSpawnPoint.rotation);
+        monsters.Add(monster);
+    }
+
+    public void MonsterAttacks(int monsterIndex)
+    {
+
+        if (monsters.Count <= monsterIndex) return;
+
+        Transform monster = monsters[monsterIndex].transform;
+        monster.GetComponent<MonsterController>().ChangeState(MonsterState.Attack);
+        monster.position = attackPoint.position;
+        monster.rotation = attackPoint.rotation;
+
+    }
+
+    float CalculateWaveDifficulty(ref float difficulty)
+    {
+        foreach (GameObject monster in monsters)
+        {
+            difficulty += monster.GetComponent<Points>().points;
+        }
+
+        difficulty /= (numberOfMonsters * 3);
+
+        return difficulty;
     }
 
     // Update is called once per frame
