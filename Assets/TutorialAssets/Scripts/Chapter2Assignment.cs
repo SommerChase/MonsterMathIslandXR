ba866f753b95f08e19542c489e30640c2af8ca8d
Chase <robertchasesommer@gmail.com>
2023-02-08 08:08:29 -0500

    ch2 assignment - not done

---
+++ Assets/TutorialAssets/Scripts/Chapter2Assignment.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Chapter2Assignment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<int> randomNumbers = new List<int>(10);
+        
+        // Capacity is different than Count. 
+        for (int index = 0; index < randomNumbers.Capacity; index++)
+        {
+            int randomInt = Random.Range(1, 100);
+            randomNumbers.Add(randomInt);
+            
+            Debug.Log(randomNumbers[index]);
+        }
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
